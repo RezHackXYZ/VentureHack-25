f7566f515dcf03ccfb3d7dea4d149d4a632a290b
RezHackXYZ <rhythmupa@gmail.com>
2025-04-27 07:48:52 +0530

    fdsdf

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -6,19 +6,17 @@
 	gsap.registerPlugin(ScrollTrigger);
 
 	onMount(() => {
-		// Create a timeline for the text reveal animations
 		const textTimeline = gsap.timeline({
 			scrollTrigger: {
 				trigger: "#ani",
 				start: "top top",
-				end: "+=300%", // Adjust this to control how long the animation pins
+				end: "+=300%",
 				pin: true,
 				scrub: 1,
-				markers: false, // Set to true during development to see markers
+				markers: false,
 			},
 		});
 
-		// Animate each text element with staggered effect
 		textTimeline
 			.to(".FrontText:nth-child(1)", { opacity: 1, y: 0, duration: 1 })
 			.to(
@@ -54,10 +52,23 @@
 	</div>
 
 	<div id="content">
-		<h2>About VentureHack'25</h2>
-		<p>More information about the hackathon goes here...</p>
+		<div id="moreInfo">
+			<h2>About VentureHack'25</h2>
+			<p>More information about the hackathon goes here...</p>
+		</div>
 	</div>
-	<div id=scroolBadge>Scrool Down To continue</div>
+	<div id="scroolBadge">
+		Scroll Down To continue<svg
+			xmlns="http://www.w3.org/2000/svg"
+			height="24px"
+			viewBox="0 -960 960 960"
+			width="24px"
+			fill="#FFFFFF"
+			><path
+				d="M180-360 40-500l42-42 70 70q-6-27-9-54t-3-54q0-82 27-159t78-141l43 43q-43 56-65.5 121.5T200-580q0 26 3 51.5t10 50.5l65-64 42 42-140 140Zm478 233q-23 8-46.5 7.5T566-131L304-253l18-40q10-20 28-32.5t40-14.5l68-5-112-307q-6-16 1-30.5t23-20.5q16-6 30.5 1t20.5 23l148 407-100 7 131 61q7 3 15 3.5t15-1.5l157-57q31-11 45-41.5t3-61.5l-55-150q-6-16 1-30.5t23-20.5q16-6 30.5 1t20.5 23l55 150q23 63-4.5 122.5T815-184l-157 57Zm-90-265-54-151q-6-16 1-30.5t23-20.5q16-6 30.5 1t20.5 23l55 150-76 28Zm113-41-41-113q-6-16 1-30.5t23-20.5q16-6 30.5 1t20.5 23l41 112-75 28Zm8 78Z"
+			/></svg
+		>
+	</div>
 </div>
 
 <style>
@@ -102,15 +113,21 @@
 		border-radius: 100px;
 		font-size: 1.2rem;
 		opacity: 0.8;
+		display: flex;
+		align-items: center;
+		gap: 2px;
 	}
 
 	#content {
 		position: relative;
 		z-index: 1;
-		background-color: #ffffff;
+		background-color: #2b2b2b;
 		min-height: 100vh;
-		padding: 2rem;
-		color: #000;
+		color: #ffffff;
+	}
+
+	#moreInfo {
+		padding: 20px;
 	}
 
 	.FrontText {
