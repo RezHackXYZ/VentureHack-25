a6b2677bf6eb027ff4818fb274da7bfde82cbd63
RezHackXYZ <rhythmupa@gmail.com>
2025-04-27 06:46:57 +0530

    first commit

---
+++ src/app.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Router from "svelte-spa-router";
+
+	import Home from "./pages/home.svelte";
+	import a from "./pages/a.svelte";
+	import b from "./pages/b.svelte";
+	import c from "./pages/c.svelte";
+	let routes = {
+		"/": Home,
+		"/a": a,
+		"/b": b,
+		"/c": c,
+	};
+
+	import Nav from "./nav.svelte";
+</script>
+
+<Nav />
+<Router {routes} />
